23e742828048e5feddf51036b0580e06acdaa38b
Vineetsh7 <xyz.meoutlook010@gmail.com>
2025-12-25 18:03:41 +0530

    Add LinkedList implementation with basic operations

---
+++ 04. LinkedList/LL.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+;
+
+Node *convertArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0], nullptr);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); ++i)
+    {
+        Node *temp = new Node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int lengthOfLL(Node *head)
+{
+    int cnt = 0;
+    Node *temp = head;
+    while (temp)
+    {
+        // cout<<temp->data<<" ";
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+};
+
+int checkPresent(Node *head, int value)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->data == value)
+            return 1;
+        temp = temp->next;
+    }
+    return 0;
+}
+
+Node *removeHead(Node *head)
+{
+    if (head == NULL)
+        return head;
+
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node *removeTail(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    Node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr{2, 5, 8, 7};
+    Node *head = convertArr2LL(arr);
+    // cout << head->data;
+    // cout << lengthOfLL(head);
+    // cout << checkPresent(head, 5);
+    // head = removeHead(head);
+    head = removeTail(head);
+    print(head);
+}
